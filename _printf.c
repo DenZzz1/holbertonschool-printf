a133494747a8c0a51ffbb40ff2dc211e8252fa07
DenZzz1 <m2nky2fficiel@gmail.com>
2025-11-27 19:50:18 +0000

    task1

--- _printf.c
+++ _printf.c
@@ -6,26 +6,86 @@ int _printf(const char *format, ...)
 {
 va_list args;
 int count = 0;
+int i, j;
+char buf[12];
+unsigned int num;
+int neg;
+
+if (format == NULL)
+return (-1);
 
 va_start(args, format);
-while (*format)
+
+i = 0;
+while (format[i] != '\0')
 {
-if (*format == '%')
+if (format[i] == '%')
 {
-format++;
-if (*format == 'd' || *format == 'i')
+i++;
+if (format[i] == 'd' || format[i] == 'i')
 {
 int n = va_arg(args, int);
-}
-
+neg = 0;
+if (n < 0)
+{
+neg = 1;
+num = (unsigned int)(-n);
 }
 else
 {
-write(1, format, 1);
-count++;
+num = (unsigned int)n;
 }
-format++;
+j = 0;
+if (num == 0)
+{
+buf[j++] = '0';
 }
+else
+{
+while (num > 0 && j < 11)
+{
+buf[j++] = (num % 10) + '0';
+num = num / 10;
+}
+}
+if (neg)
+count += write(1, "-", 1);
+while (j > 0)
+{
+count += write(1, &buf[j - 1], 1);
+j--;
+}
+}
+else if (format[i] == 'c')
+{
+char c = (char)va_arg(args, int);
+count += write(1, &c, 1);
+}
+else if (format[i] == 's')
+{
+char *str = va_arg(args, char *);
+if (str == NULL)
+str = "(null)";
+for (j = 0; str[j] != '\0'; j++)
+count += write(1, &str[j], 1);
+}
+else if (format[i] == '%')
+{
+count += write(1, "%", 1);
+}
+else
+{
+count += write(1, "%", 1);
+count += write(1, &format[i], 1);
+}
+}
+else
+{
+count += write(1, &format[i], 1);
+}
+i++;
+}
+
 va_end(args);
-return count;
+return (count);
 }
